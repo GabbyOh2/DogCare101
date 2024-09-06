a56d102dbcdd8b2c5e6da3cd1612229cb5e95e41
Gabrielle Oh <156555529+GabbyOh2@users.noreply.github.com>
2024-09-06 09:07:53 -0400

    Add files via upload

---
+++ LMC2700-Website/script.js
@@ -0,0 +1,9 @@
+// Example JS for button click, add any interactivity needed here
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
+
+
+
